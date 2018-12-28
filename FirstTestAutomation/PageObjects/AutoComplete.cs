afbcc8206d21f61ccddbf8dd23c7b3c5de5310bf
smitha <smithajojo@gmail.com>
2018-12-28 10:45:24 +0000

    First Test Method execution

---
+++ FirstTestAutomation/PageObjects/AutoComplete.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System.Threading;
+
+namespace FirstTestAutomation.PageObjects
+{
+    class AutoComplete
+    {
+        private readonly IWebDriver _driver;
+
+        public AutoComplete(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(_driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "autocomplete")]
+        private IWebElement autocompletetextbox;
+
+        [FindsBy(How = How.ClassName, Using = "pac-item")]
+        private IWebElement autocompleteselector;
+
+        public void FillTextbox()
+        {
+            autocompletetextbox.Clear();
+            autocompletetextbox.SendKeys("1280 Massachusetts Avenue, Cambridge, MA, USA");
+            Thread.Sleep(1000);
+            autocompleteselector.Click();
+        }
+
+        public void LoadForm()
+        {
+            _driver.Url = "http://formy-project.herokuapp.com/autocomplete";
+        }
+    }
+}
--- FirstTestAutomation/Startup.cs
+++ FirstTestAutomation/Startup.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FirstTestAutomation
 {
     public class Startup
     {
+        [NUnit.Framework.TestCase]
+        public void ExecuteAutoCompleteTest()
+        {
+            IWebDriver webdriver = new ChromeDriver();
+            PageObjects.AutoComplete autoComplete = new PageObjects.AutoComplete(webdriver);
+            autoComplete.LoadForm();
+            autoComplete.FillTextbox();
+            Thread.Sleep(2000);
+            webdriver.Close();
+        }
     }
 }
